13bcdeb089a1a5e21de86c213ab5110d18a40625
Sofia Prenner <prenner.s@wustl.edu>
2025-10-01 13:28:27 -0500

    sofia flag

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -1,11 +1,24 @@
 package studio4;
 
 import java.awt.Color;
+import java.io.File;
+import java.util.Scanner;
+
+import javax.rmi.ssl.SslRMIClientSocketFactory;
+import javax.swing.JFileChooser;
 
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
+		double x = 0.5;
+		double y = 0.5;
+		double halfWidth = 0.4;
+		double halfHeight = 0.2;
+		StdDraw.setPenColor(StdDraw.MAGENTA);
+		StdDraw.filledRectangle(x, y, halfWidth, halfHeight);
+
+		StdDraw.setPenColor(StdDraw.BOOK_LIGHT_BLUE);
+		StdDraw.filledEllipse(0.5, 0.5, 0.1, 0.1);
 	}
 }
