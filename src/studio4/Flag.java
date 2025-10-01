618b594b2004d3926790026097d141b6fc461ff9
Sofia Prenner <prenner.s@wustl.edu>
2025-10-01 13:39:02 -0500

    flag 2.0

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -15,10 +15,16 @@ public class Flag {
 		double y = 0.5;
 		double halfWidth = 0.4;
 		double halfHeight = 0.2;
+		double[] xTri = {0.3, 0.7, 0.5};
+		double[] yTri = {0.4, 0.4, Math.sqrt(1.0)/2.0};
+
 		StdDraw.setPenColor(StdDraw.MAGENTA);
 		StdDraw.filledRectangle(x, y, halfWidth, halfHeight);
 
 		StdDraw.setPenColor(StdDraw.BOOK_LIGHT_BLUE);
 		StdDraw.filledEllipse(0.5, 0.5, 0.1, 0.1);
+
+		StdDraw.setPenColor(192, 192, 192);
+		StdDraw.filledPolygon(xTri, yTri);
 	}
 }
